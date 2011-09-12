013e6d530ae624169bb4983d44911850a29b2ebd
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-09-12 10:26:59 +0200

    move reg parsing to NetFPGA

--- netfpga.rb
+++ netfpga.rb
@@ -1,23 +1,30 @@
 require 'ffi'
+require './reg_parser'
 
 system 'gcc -c -fPIC netfpga-regset.c'                     unless File.exists? 'netfpga-regset.o'
 system 'gcc -shared -o netfpga-regset.so netfpga-regset.o' unless File.exists? 'netfpga-regset.so'
 
-module NetFPGA
+class NetFPGA
   extend FFI::Library
   ffi_lib './netfpga-regset.so'
   attach_function :get_register, [:uint],        :uint
   attach_function :set_register, [:uint, :uint], :void
 
-  def self.get reg
-    self.get_register $regvals[reg]
+  def initialize file
+    @registers = RegParser.parse file
   end
 
-  def self.set reg, val
-    self.set_register $regvals[reg], val
+  def get reg
+    get_register @registers[reg]
+  end
+
+  def set reg, val
+    set_register @registers[reg], val
   end
 end
 
-def NetFPGA.set_register reg, val
-  puts "setting #{$regvals.invert[reg]} to val #{val}"
-end unless File.exists? '/sys/class/net/nf2c0'
+class NetFPGA
+  def set_register reg, val
+    puts "setting #{@registers.invert[reg]} to val #{val}"
+  end unless File.exists? '/sys/class/net/nf2c0'
+end
